4ba2563bf22546e54a60b68765d66fcb55c97daf
Rafael Collado <rafaelcolladojr@gmail.com>
2022-01-17 01:31:43 -0500

    Moved custom appbar code to actual Scaffold AppBar widget.

--- lib/features/roadmap/presentation/pages/home_page.dart
+++ lib/features/roadmap/presentation/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:refold_pocket_roadmap/config/config.dart';
 import 'package:refold_pocket_roadmap/core/util/app_colors.dart';
 import 'package:refold_pocket_roadmap/core/util/app_text_styles.dart';
@@ -12,114 +13,112 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    MediaQueryData deviceInfo = MediaQuery.of(context);
-    return Scaffold(
-      body: SafeArea(
-        child: DefaultTabController(
-          length: kAvailableRoadmaps.length,
-          child: Container(
-            color: kPrimaryColorDark.withAlpha(70),
-            child: Column(
-              children: [
-                Material(
-                  color: Colors.white,
-                  elevation: 4.0,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        opacity: 0.2,
-                        image: AssetImage('assets/images/logo-primary.png'),
-                        alignment: Alignment.topLeft,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(8.0),
-                    height: deviceInfo.size.height * 0.25,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                                onPressed: () {},
-                                icon: const Icon(
-                                  Icons.menu,
-                                  color: kPrimaryColorDark,
-                                  size: 30.0,
-                                )),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Text("Pocket Roadmaps",
-                              style: kTitle1.withColor(kPrimaryColorDark)),
-                        ),
-                        TabBar(
-                          isScrollable: true,
-                          indicator: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: kPrimaryColorDark,
-                          ),
-                          unselectedLabelColor: kPrimaryColorDark,
-                          tabs: kAvailableRoadmaps.keys
-                              .map((lang) => Tab(text: lang.toLargeString()))
-                              .toList(),
-                        ),
-                      ],
+    Size deviceSize = MediaQuery.of(context).size;
+    return DefaultTabController(
+      length: kAvailableRoadmaps.length,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.menu,
+                color: kPrimaryColorDark,
+                size: 30.0,
+              ),
+            ),
+          ],
+          bottom: PreferredSize(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: Text(
+                      'Pocket\nRoadmaps',
+                      style: kTitle1.withColor(kPrimaryColorDark),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: TabBarView(
+                  TabBar(
+                    isScrollable: true,
+                    indicator: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: kPrimaryColorDark,
+                    ),
+                    unselectedLabelColor: kPrimaryColorDark,
+                    tabs: kAvailableRoadmaps.keys
+                        .map((lang) => Tab(text: lang.toLargeString()))
+                        .toList(),
+                  ),
+                ],
+              ),
+            ),
+            preferredSize: Size.fromHeight(deviceSize.height * 0.15),
+          ),
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/logo-primary.png'),
+                opacity: 0.2,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+        ),
+        body: SafeArea(
+          bottom: false,
+          child: Container(
+            color: kPrimaryColorDark.withAlpha(70),
+            child: Expanded(
+              child: TabBarView(
+                children: [
+                  ListView(
                     children: [
-                      ListView(
-                        children: [
-                          RoadmapListItem(
-                            title: "Detailed Roadmap",
-                            onTap: () {
-                              Navigator.pushNamed(context, RoadmapPage.route);
-                            },
-                          ),
-                          const RoadmapListItem(
-                            title: "Simplified Roadmap",
-                            enabled: false,
-                          ),
-                        ],
+                      RoadmapListItem(
+                        title: "Detailed Roadmap",
+                        onTap: () {
+                          Navigator.pushNamed(context, RoadmapPage.route);
+                        },
                       ),
-                      ListView(
-                        children: const [
-                          RoadmapListItem(
-                            title: "Quickstart Guide",
-                            enabled: false,
-                            backgroundImage:
-                                AssetImage('assets/images/flag-es.png'),
-                          ),
-                          RoadmapListItem(
-                            title: "Simplified Roadmap",
-                            enabled: false,
-                          ),
-                        ],
-                      ),
-                      Center(
-                        child: Text(
-                          'Coming Soon',
-                          style: kTitle2
-                              .withColor(kPrimaryColorDarkExtra.withAlpha(100)),
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          'Coming Soon',
-                          style: kTitle2
-                              .withColor(kPrimaryColorDarkExtra.withAlpha(100)),
-                        ),
+                      const RoadmapListItem(
+                        title: "Simplified Roadmap",
+                        enabled: false,
                       ),
                     ],
                   ),
-                ),
-              ],
+                  ListView(
+                    children: const [
+                      RoadmapListItem(
+                        title: "Quickstart Guide",
+                        enabled: false,
+                        backgroundImage:
+                            AssetImage('assets/images/flag-es.png'),
+                      ),
+                      RoadmapListItem(
+                        title: "Simplified Roadmap",
+                        enabled: false,
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Text(
+                      'Coming Soon',
+                      style: kTitle2
+                          .withColor(kPrimaryColorDarkExtra.withAlpha(100)),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Coming Soon',
+                      style: kTitle2
+                          .withColor(kPrimaryColorDarkExtra.withAlpha(100)),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
