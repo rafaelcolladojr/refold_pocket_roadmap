2f487b48c2b7fcdb45c9dd902ee57d693f52544b
Rafael Collado <rafaelcolladojr@gmail.com>
2022-01-26 16:23:50 -0500

    Implemented Bloc for HomePage. Set up Bloc-related classes for following RoadmapPage.

--- lib/features/roadmap/presentation/bloc/roadmap/roadmap_bloc.dart
+++ lib/features/roadmap/presentation/bloc/roadmap/roadmap_bloc.dart
@@ -1,1 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:refold_pocket_roadmap/core/error/failure.dart';
+import 'package:refold_pocket_roadmap/core/params/get_article_params.dart';
+import 'package:refold_pocket_roadmap/core/params/get_roadmap_params.dart';
+import 'package:refold_pocket_roadmap/features/roadmap/domain/entity/article_entity.dart';
+import 'package:refold_pocket_roadmap/features/roadmap/domain/entity/roadmap_entity.dart';
+import 'package:refold_pocket_roadmap/features/roadmap/domain/repository/roadmap_repository.dart';
+import 'package:refold_pocket_roadmap/features/roadmap/presentation/bloc/roadmap/roadmap_event.dart';
+import 'package:refold_pocket_roadmap/features/roadmap/presentation/bloc/roadmap/roadmap_state.dart';
 
+class RoadmapBloc extends Bloc<RoadmapEvent, RoadmapState> {
+  RoadmapBloc(RoadmapRepository roadmapRepository)
+      : _roadmapRepository = roadmapRepository,
+        super(RoadmapState());
+
+  final RoadmapRepository _roadmapRepository;
+
+  Future<void> _onGetRoadmapEvent(GetRoadmapEvent event, Emitter<RoadmapState> emitter) async {
+    Either<Failure, Roadmap> result = await _roadmapRepository.getRoadmap(GetRoadmapParams(type: event.type, lang: event.lang));
+    result.fold(
+      (left) => null,
+      (right) => null,
+    );
+  }
+
+  Future<void> _onGetArticleEvent(GetArticleEvent event, Emitter<RoadmapState> emitter) async {
+    Either<Failure, Article> roadmap = await _roadmapRepository.getArticle(GetArticleParams(id: event.id));
+  }
+}
